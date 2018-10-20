f9945988cb79f55c0284ec22ecc7e651fb3013a3
Pablo Cantero <pablohstc@gmail.com>
2018-10-20 17:53:51 -0400

    "prettierfy"

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import { Application } from 'probot'
+import { createIssue } from './securityAlertHandler'
 
 export = (app: Application) => {
   const SECURITY = 'security'
 
-  app.on('repository_vulnerability_alert.create', async (context) => {
+  app.on('repository_vulnerability_alert.create', async context => {
     const alert = context.payload.alert
-    const title = `Security vulnerability found in ${alert.affected_package_name}`
+    const title = `Security vulnerability found in ${
+      alert.affected_package_name
+    }`
 
     const body = `- Affected version ${alert.affected_package_name} ${
       alert.affected_range
     } \n- Fixed in ${alert.fixed_in}`
-  })
 
-  await github.issues.create({
-    owner,
-    repo,
-    title,
-    body,
-    labels: [SECURITY]
+    await github.issues.create({
+      owner,
+      repo,
+      title,
+      body,
+      labels: [SECURITY]
+    })
   })
 }
