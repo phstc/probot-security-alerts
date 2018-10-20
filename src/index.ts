fb31b8f683ffc248bf78f5a8d63dc010fcf394dd
Pablo Cantero <pablohstc@gmail.com>
2018-10-20 18:28:42 -0400

    Add heroku-postbuild for compiling ts
    See https://devcenter.heroku.com/articles/nodejs-support
    
    Found in https://github.com/offu/close-issue-app/blob/9515f1076204429203746116af0748cfaa35d51c/package.json#L13

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,5 @@ import { Application } from 'probot'
 import { createIssue } from './securityAlertHandler'
 
 export = (app: Application) => {
-  const SECURITY = 'security'
-
-  app.on('repository_vulnerability_alert.create', async context => {
-    const alert = context.payload.alert
-    const title = `Security vulnerability found in ${
-      alert.affected_package_name
-    }`
-
-    const body = `- Affected version ${alert.affected_package_name} ${
-      alert.affected_range
-    } \n- Fixed in ${alert.fixed_in}`
-
-    await github.issues.create({
-      owner,
-      repo,
-      title,
-      body,
-      labels: [SECURITY]
-    })
-  })
+  app.on('repository_vulnerability_alert.create', async context => createIssue(context))
 }
